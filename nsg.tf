ac803066f13adbb44e7e5153c94d30aa79539d0c
daatlk <darshana.athukorala@ifsworld.com>
2021-09-22 16:20:52 +0530

    added more resources

---
+++ nsg.tf
@@ -0,0 +1,20 @@
+resource "azurerm_network_security_group" "nsg-1" {
+  name                = "security-group-1"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resource_group-1.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = var.tags
+}
